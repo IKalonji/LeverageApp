0dd564f295e2022996b773725ea7bc46f1048049
Issa Kalonji <issa@bbd.co.za>
2021-08-02 15:51:56 +0200

    Added community UI elements.

--- leverageApp/src/app/founders-network-tabs-view-community/founders-network-tabs-view-community.page.ts
+++ leverageApp/src/app/founders-network-tabs-view-community/founders-network-tabs-view-community.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-founders-network-tabs-view-community',
@@ -7,9 +8,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoundersNetworkTabsViewCommunityPage implements OnInit {
 
+  
+  post = [{
+    from: "@IKalonji",
+    message: "I need assistance with formulatging a business strategy, is there anyone with experience that can assist",
+    showReply: false,
+    replies: [{
+      from: "LZaze",
+      message: "Hey, I'll be able to assist. What you need to do is a simple swot analysis on your potential business and take the next steps based on the weaknesses and threats"
+      },
+      {
+      from: "JMampa",
+      message: "Just start you'll figure the rest"
+      }
+    ]
+  },
+  {
+    from: "@IKalonji",
+    message: "I need assistance with formulatging a business strategy, is there anyone with experience that can assist",
+    showReply: false,
+    replies: [{
+      from: "LZaze",
+      message: "Hey, I'll be able to assist. What you need to do is a simple swot analysis on your potential business and take the next steps based on the weaknesses and threats"
+      },
+      {
+      from: "JMampa",
+      message: "Just start you'll figure the rest"
+      }
+    ]
+  },
+  {
+    from: "@IKalonji",
+    message: "I need assistance with formulatging a business strategy, is there anyone with experience that can assist",
+    showReply: false,
+    replies: [{
+      from: "LZaze",
+      message: "Hey, I'll be able to assist. What you need to do is a simple swot analysis on your potential business and take the next steps based on the weaknesses and threats"
+      },
+      {
+      from: "JMampa",
+      message: "Just start you'll figure the rest"
+      }
+    ]
+  },
+  ]
+
+  replies = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  async onClick(){
+    const toast = await new ToastController().create(
+      {
+        message: "Replied",
+        duration: 2000
+      }
+    );
+    await toast.present()
+  }
+
+  viewReplies(i){
+    i.showReply = !i.showReply;
   }
 
 }
